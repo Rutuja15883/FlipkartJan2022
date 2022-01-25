e65e7f42aeef5f893eb3dd19ef9f1db5a84671c9
Rutuja15883 <rutujasaipawar15@gmail.com>
2022-01-25 13:11:38 +0530

    By Kadambari

---
+++ src/test/java/support/manageTrial.java
@@ -0,0 +1,11 @@
+package support;
+
+public class manageTrial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good morning");
+		System.out.println("Good day to every one");
+	}
+
+}
